e3b4858afb546cb35a93735e983a8d6945c2827c
Mads Albertsen <MadsAlbertsen85@gmail.com>
2013-02-27 20:50:51 +0100

    updated links

--- R.data.generation/workflow.R.data.generation.sh
+++ R.data.generation/workflow.R.data.generation.sh
@@ -5,7 +5,6 @@
 
 ##### Needed input files
 # Fasta file with all assembled scaffolds (keep the naming as >1, >2 etc): assembly.fa
-# HMM models for the essential proteins: essential.hmm
 
 ##### Needed software
 # Prodigal
@@ -19,11 +18,11 @@ echo "---Metagenomics workflow script v.1.0---"
 
 echo ""
 echo "Calculating tetranucleotide frequency"
-perl multi-metagenome/misc.scripts/calc.kmerfreq.pl -i assembly.fa -o assembly.kmer.tab
+perl multi-metagenome/R.data.generation/calc.kmerfreq.pl -i assembly.fa -o assembly.kmer.tab
 
 echo ""
 echo "Calculating gc content"
-perl multi-metagenome/misc.scripts/calc.gc.pl -i assembly.fa -o assembly.gc.tab
+perl multi-metagenome/R.data.generation/calc.gc.pl -i assembly.fa -o assembly.gc.tab
 
 echo ""
 echo "Finding essential genes - Predicting proteins (Prodigal)"
@@ -32,10 +31,10 @@ cut -f1 -d " " temp.orfs.faa > assembly.orfs.faa
 
 echo ""
 echo "Finding essential genes - running HMM search"
-hmmsearch --tblout assembly.hmm.orfs.txt --cut_tc --notextw essential.hmm assembly.orfs.faa > hmm.temp.txt
+hmmsearch --tblout assembly.hmm.orfs.txt --cut_tc --notextw multi-metagenome/R.data.generation/essential.hmm assembly.orfs.faa > hmm.temp.txt
 tail -n+4  assembly.hmm.orfs.txt | sed 's/ * / /g' | cut -f1,4 -d " " | sed 's/_/ /' > assembly.orfs.hmm.id.txt
 grep -v "#" assembly.hmm.orfs.txt | cut -f1 -d " " > list.of.positive.orfs.txt
-perl multi-metagenome/misc.scripts/extract.using.header.list.pl -l list.of.positive.orfs.txt -s assembly.orfs.faa -o assembly.orfs.hmm.faa
+perl multi-metagenome/R.data.generation/extract.using.header.list.pl -l list.of.positive.orfs.txt -s assembly.orfs.faa -o assembly.orfs.hmm.faa
 
 echo ""
 echo "Finding essential genes - Blasting positive hits"
@@ -44,7 +43,7 @@ blastp -query assembly.orfs.hmm.faa -db refseq_protein -evalue 1e-5 -num_threads
 echo ""
 echo "Finding essential genes - Extracting consensus taxonomic assignment"
 MEGAN +g -x "import blastfile= assembly.orfs.hmm.blast.xml meganfile=temp.rma;recompute toppercent=5;recompute minsupport=1;update;collapse rank=Species;update;select nodes=all;export what=CSV format=readname_taxonpath separator=tab file=assembly.orfs.hmm.blast.tax.txt;update;close"
-perl multi-metagenome/misc.scripts/hmm.majority.vote.pl -i assembly.orfs.hmm.blast.tax.txt -o assembly.tax.consensus.txt -n
+perl multi-metagenome/R.data.generation/hmm.majority.vote.pl -i assembly.orfs.hmm.blast.tax.txt -o assembly.tax.consensus.txt -n
 sed 's/\t/;/' assembly.orfs.hmm.blast.tax.txt | cut -f1,5 -d ";" | sed 's/;/\t/' | sed 's/_/\t/'  > assembly.orfs.hmm.blast.tax.tab
 
 #echo ""
